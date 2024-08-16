71e2ade763f90570f0079214e3eb7fa004ef6c0a
imsherlocked9102 <83703937+imsherlocked9102@users.noreply.github.com>
2024-08-16 11:18:41 +0530

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,29 +1,35 @@
-(function() {
+(function(){
 'use strict';
+angular.module('LunchChecker',[])
+.controller('LunchCheckerController',LunchCheckerController);
+LunchCheckerController.$inject=['$scope'];
 
-angular.module('appModule1', [])
+function LunchCheckerController($scope){
+$scope.checkDishes=function(){
+	var num =countDishes($scope.dishes);
+	$scope.message=createmessage(num);
+};
+function countDishes(dishes) {
+	var count=0;
+	if(dishes){
+		var arr=dishes.split(',');
+		for(var i in arr){
+			if(arr[i].trim().length!=0){
+				count++;
+			}
+		}
+	}
+	return count;
+}
+function createmessage(num) {
+	if(num==0)
+		return "Please enter data first";
+	else if(num<4)
+		return 'Enjoy!';
+	else
+		return 'Too much!';
+}
 
-    .controller('appControllerModule1', function($scope) {
-        
-        $scope.items = [];
-        $scope.message = '';
-        $scope.classChecker = null;
-        $scope.whiteSpaceChecker = false;
-
-        $scope.getItems = function() {
-            if (!$scope.items || !$scope.items.length) {
-                $scope.message = 'Please enter data first.';
-                $scope.classChecker = false;
-            } else {
-                var tempTotal = $scope.items.split(',');
-                $scope.classChecker = true;
-                    if (tempTotal.length > 3) {
-                        $scope.message = 'Too much!';
-                    } else {
-                        $scope.message = 'Enjoy';
-                    }
-            }
-        }
-    });
+}
 
 })();
